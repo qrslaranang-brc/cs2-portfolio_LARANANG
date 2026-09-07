e2ca6072dff7d250b239e3213add006aa281eb38
qrslaranang-brc <qrslaranang@brc.pshs.edu.ph>
2026-09-07 20:06:52 +0800

    Update score_checker.py

--- Q1/score_checker.py
+++ Q1/score_checker.py
@@ -1,1 +1,11 @@
-
+student_score = int(input("Enter student score: "))
+if student_score < 0 or student_score > 100:
+  print("Invalid Score.")
+elif student_score >= 90:
+  print("outstanding")
+elif student_score >=80:
+  print("very satisfactory")
+elif student_score >=75:
+  print("satisfactory")
+else:
+  print("needs improvement")
